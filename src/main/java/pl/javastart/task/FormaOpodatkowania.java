64d3747bf4b42e041076743c383558cc560412af
Krzysiek Przystarz <frodson35@gmail.com>
2023-01-13 18:51:58 +0100

    chyba muszę jeszcze raz przerobić polimorfizm, bo z tym zadaniem strasznie słabo mi idzie... nie mam kompletnie pomysłu co zrobić dalej, jak wyliczyć wydatki, przychody... Generalnie zauważyłem, że mam problem z zadaniami w których nie buduję od podstaw, tylko muszę coś uzupełniać... Nie chcę się poddawać, bo jeszcze mam sporo czasu do niedzieli, więc jeśli byłbyś w stanie jakoś mnie nakierować, to będę wdzięczny

--- src/main/java/pl/javastart/task/FormaOpodatkowania.java
+++ src/main/java/pl/javastart/task/FormaOpodatkowania.java
@@ -1,9 +1,30 @@
 package pl.javastart.task;
 
-public class FormaOpodatkowania {
+public class FormaOpodatkowania extends Firma {
+
+    static final int KOWTA_WOLNA_OD_PODATKU1 = 10_000;
+    static final int KOWTA_WOLNA_OD_PODATKU2 = 100_000;
+    static final double PIERWSZY_PROG_PODATKU = 0.18;
+    static final double DRUGI_PROG_PODATKU = 0.32;
+
+    public FormaOpodatkowania(String nazwa, FormaOpodatkowania formaOpodatkowania) {
+        super(nazwa, formaOpodatkowania);
+    }
 
     public double wyliczPodatek(double przychody) {
-        System.out.println("Metoda z wyliczaniem podatku nie została nadpisana");
-        return -1;
+        if (przychody < KOWTA_WOLNA_OD_PODATKU1) {
+            return przychody;
+        }
+        if (przychody < KOWTA_WOLNA_OD_PODATKU2) {
+            return (PIERWSZY_PROG_PODATKU * przychody) + KOWTA_WOLNA_OD_PODATKU1;
+        }
+        if (przychody > KOWTA_WOLNA_OD_PODATKU2) {
+            return (DRUGI_PROG_PODATKU * przychody) + KOWTA_WOLNA_OD_PODATKU1;
+        }
+        return przychody;
+    }
+
+    private double zsumujPrzychody() {
+        return 1;
     }
 }
